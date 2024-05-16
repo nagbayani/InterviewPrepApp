60946e10f7cdacfbe313da912988fe7d8f7d0bfc
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-05-15 16:49:14 -1000

    Test files deleted

--- interview-prep-app/src/lib/test.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { auth } from "@/lib/testAuth";
-
-export const GET = async (req: Request, res: NextResponse) => {
-  const session = await auth();
-
-  return NextResponse.json({ authenticated: !!session });
-};
