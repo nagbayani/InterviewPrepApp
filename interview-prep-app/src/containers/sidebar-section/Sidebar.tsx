5b15e1d1b1a07bd33a984b7f1341fde04e712158
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-08-13 20:50:56 -0700

    Responsive Sidebar changes

--- interview-prep-app/src/containers/sidebar-section/Sidebar.tsx
+++ interview-prep-app/src/containers/sidebar-section/Sidebar.tsx
@@ -20,9 +20,15 @@ const Sidebar = () => {
   if (!sidebar) return null;
 
   return (
+    // <aside
+    //   className={cn(
+    //     "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+    //     sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+    //   )}
+    // >
     <aside
       className={cn(
-        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+        "fixed top-0 left-0 z-20 h-screen lg:translate-x-0 transition-[width] ease-in-out duration-300",
         sidebar?.isOpen === false ? "w-[90px]" : "w-72"
       )}
     >
