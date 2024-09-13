78a71cd3fabe3b3ad589bb2b1a11e2b0d2948678
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-09-12 17:55:22 -0700

    Stripe Config

--- interview-prep-app/prisma/migrations/20240419000824_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "email" TEXT NOT NULL
-);
--- interview-prep-app/prisma/migrations/20240906001554_updated_interview_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Interview" ADD COLUMN "qualifications" TEXT;
-ALTER TABLE "Interview" ADD COLUMN "skills" TEXT;
